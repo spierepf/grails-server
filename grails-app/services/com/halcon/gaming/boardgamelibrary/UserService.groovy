f3dc8ef9f74f593af46929549c2cc0d430501f1f
Peter-Frank Spierenburg <peter@hal-con.com>
2023-08-30 09:46:25 -0300

    ./grailsw generate-controller User

---
+++ grails-app/services/com/halcon/gaming/boardgamelibrary/UserService.groovy
@@ -0,0 +1,18 @@
+package com.halcon.gaming.boardgamelibrary
+
+import grails.gorm.services.Service
+
+@Service(User)
+interface UserService {
+
+    User get(Serializable id)
+
+    List<User> list(Map args)
+
+    Long count()
+
+    User delete(Serializable id)
+
+    User save(User user)
+
+}
